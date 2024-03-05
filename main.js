9fa5debc8f49d11eb39752876eea563c0791727d
Coding WIth Ahmed <155929233+ahmedprogrammer2005@users.noreply.github.com>
2024-03-06 02:08:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// Assignment 3
+
+// Answer 1
+// var variable1, variable2, variable3;
+
+// Answer 2
+// Legal variable names:
+
+// firstName
+// age
+// user_id
+// myVar
+// _myVariable
+// Illegal variable names:
+
+// 123variable (starts with a number)
+// my variable (contains a space)
+// user-id (contains a hyphen, which is not allowed)
+// var (reserved keyword)
+// !illegalVariable (contains a special character other than underscore)
+// In JavaScript, variable names must follow these rules:
+
+// They can contain letters, digits, underscores, and dollar signs.
+// They must begin with a letter, underscore (_), or dollar sign ($).
+// Variable names are case-sensitive.
+// They cannot be reserved keywords, such as "var", "if", "for", etc.
+
+// Answer 3
+document.write("<h1>Rules for Naming JS Variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name</p>");
+document.write("<p>Variable names are case-sensitive</p>");
+document.write("<p>Variable names should not be JS reserved keywords</p>");
